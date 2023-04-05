60cde94f15899f259e840629b5b5823982ffa08b
shui4 <1078185271@qq.com>
2023-04-05 22:38:30 +0800

    feat(5.3.x-learn): 依赖注入

--- thinking-in-spring/src/main/java/indi/shui4/thinking/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
+++ thinking-in-spring/src/main/java/indi/shui4/thinking/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
@@ -3,6 +3,7 @@ package indi.shui4.thinking.spring.ioc.overview.dependency.injection;
 import indi.shui4.thinking.spring.ioc.overview.repository.UserRepository;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.env.Environment;
 
@@ -12,7 +13,7 @@ import org.springframework.core.env.Environment;
  * 案例：
  * <ol>
  *     <li>自动依赖注入。</li>
- *     <li>通过依赖注入获取的BeanFactory与当前应用的BeanFactory不相等，但是通过{@link ApplicationContext}进行比较是相等的，因为依赖注入的方式并没有想象的那样简单。</li>
+ *     <li>通过依赖注入获取的{@link BeanFactory}与当前应用的{@link ClassPathXmlApplicationContext}不相等，因为取的是 {@link AbstractApplicationContext#setParent(ApplicationContext)}。</li>
  *     <li>通过依赖查找直接获取{@link BeanFactory}内置依赖获取失败，但{@link Environment}内置依赖却是可以的。</li>
  * </ol>
  * </p>
@@ -44,5 +45,7 @@ public class DependencyInjectionDemo {
         System.out.println("获取Environment类型Bean:" + environment);
     }
 
+    private static void whoIsIocContainer(UserRepository userRepository, BeanFactory beanFactory) {
 
+    }
 }
