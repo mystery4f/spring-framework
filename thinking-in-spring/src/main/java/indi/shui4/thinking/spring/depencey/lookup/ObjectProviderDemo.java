6a1b82cb6c807a280a29f50a656a0185bdba23ff
shui4 <1078185271@qq.com>
2023-04-16 10:35:53 +0800

    feat(5.3.x-learn): 1

--- thinking-in-spring/src/main/java/indi/shui4/thinking/spring/depencey/lookup/ObjectProviderDemo.java
+++ thinking-in-spring/src/main/java/indi/shui4/thinking/spring/depencey/lookup/ObjectProviderDemo.java
@@ -1,8 +1,10 @@
 package indi.shui4.thinking.spring.depencey.lookup;
 
+import indi.shui4.thinking.spring.ioc.overview.domain.User;
 import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 /**
  * 通过   {@link ObjectProvider}  作为IoC依赖查找
@@ -17,8 +19,25 @@ public class ObjectProviderDemo {
 
 
         lookupByObjectProvider(applicationContext);
-
+        lookupIfAvailable(applicationContext);
+        lookupByStreamOps(applicationContext);
         applicationContext.close();
+    }
+
+    private static void lookupIfAvailable(AnnotationConfigApplicationContext applicationContext) {
+        ObjectProvider<User> userObjectProvider = applicationContext.getBeanProvider(User.class);
+        User user = userObjectProvider.getIfAvailable(User::createUser);
+        System.out.println("当前 User 对象:" + user);
+    }
+
+    private static void lookupByStreamOps(AnnotationConfigApplicationContext applicationContext) {
+        ObjectProvider<String> stringObjectProvider = applicationContext.getBeanProvider(String.class);
+        /*Iterator<String> iterator = stringObjectProvider.iterator();
+        while (iterator.hasNext()) {
+            String next = iterator.next();
+            System.out.println(next);
+        }*/
+        stringObjectProvider.forEach(System.out::println);
     }
 
     private static void lookupByObjectProvider(AnnotationConfigApplicationContext applicationContext) {
@@ -27,7 +46,14 @@ public class ObjectProviderDemo {
     }
 
     @Bean
+    @Primary
     public String helloWorld() {
-        return "hello world.";
+        return "Hello,World";
     }
+
+    @Bean
+    public String message() {
+        return "Message";
+    }
+
 }
