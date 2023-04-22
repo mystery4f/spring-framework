a3b6f1e0ec66ea35abd8ac658914979d516119d3
shui4 <1078185271@qq.com>
2023-04-22 10:35:03 +0800

    feat(5.3.x-learn): 1

--- thinking-in-spring/src/main/java/indi/shui4/thinking/spring/depencey/injection/AnnotationDependencyInjectionResolutionDemo.java
+++ thinking-in-spring/src/main/java/indi/shui4/thinking/spring/depencey/injection/AnnotationDependencyInjectionResolutionDemo.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.Map;
+
 /**
  * 注解驱动的依赖注入处理过程
  *
@@ -12,14 +14,18 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  */
 public class AnnotationDependencyInjectionResolutionDemo {
     // org.springframework.beans.factory.support.DefaultListableBeanFactory.resolveDependency
-    @Autowired // 依赖查找（处理）
-    private User user;  // DependencyDescriptor ->
     // 必须 (required=true)
     // 实时注入 (eager=true)
     // 通过类型（User.class）
     // 依赖查找（处理）
     // 字段名称（"user"）
     // 是否首要 (primary=true)
+    @Autowired // 依赖查找（处理）
+    private User user;  // DependencyDescriptor ->
+
+    // 集合类型依赖注入
+    @Autowired
+    private Map<String, User> users;
 
 
     public static void main(String[] args) {
@@ -31,7 +37,8 @@ public class AnnotationDependencyInjectionResolutionDemo {
 
         applicationContext.refresh();
         AnnotationDependencyInjectionResolutionDemo demo = applicationContext.getBean(AnnotationDependencyInjectionResolutionDemo.class);
-        System.out.println(demo.user);
+        System.out.println("demo.user:" + demo.user);
+        System.out.println("demo.users:" + demo.users);
         applicationContext.close();
     }
 
