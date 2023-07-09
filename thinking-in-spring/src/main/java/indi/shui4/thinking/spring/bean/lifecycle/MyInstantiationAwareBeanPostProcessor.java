094e92f3de84f9bb19a1405a4eef72555e2496f7
shui4 <1078185271@qq.com>
2023-07-09 20:10:26 +0800

    feat: 1

--- thinking-in-spring/src/main/java/indi/shui4/thinking/spring/bean/lifecycle/MyInstantiationAwareBeanPostProcessor.java
+++ thinking-in-spring/src/main/java/indi/shui4/thinking/spring/bean/lifecycle/MyInstantiationAwareBeanPostProcessor.java
@@ -1,4 +1,3 @@
-
 package indi.shui4.thinking.spring.bean.lifecycle;
 
 
@@ -11,88 +10,90 @@ import org.springframework.beans.factory.config.InstantiationAwareBeanPostProces
 import org.springframework.util.ObjectUtils;
 
 /**
- * TODO
- *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
- * @since
  */
 class MyInstantiationAwareBeanPostProcessor implements InstantiationAwareBeanPostProcessor {
 
-    @Override
-    public Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException {
-        if (ObjectUtils.nullSafeEquals("superUser", beanName) && SuperUser.class.equals(beanClass)) {
-            // 把配置完成 superUser Bean 覆盖
-            return new SuperUser();
-        }
-        return null; // 保持 Spring IoC 容器的实例化操作
-    }
+	public static final String BEAN_NAME_USER_HOLDER = "userHolder";
 
-    @Override
-    public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
-        if (ObjectUtils.nullSafeEquals("user", beanName) && User.class.equals(bean.getClass())) {
-            User user = (User) bean;
-            user.setId(2L);
-            user.setName("shui4");
-            // "user" 对象不允许属性赋值（填入）（配置元信息 -> 属性值）
-            return false;
-        }
-        return true;
-    }
+	@Override
+	public Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException {
+		if (ObjectUtils.nullSafeEquals("superUser", beanName) && SuperUser.class.equals(beanClass)) {
+			// 把配置完成 superUser Bean 覆盖
+			return new SuperUser();
+		}
+		return null; // 保持 Spring IoC 容器的实例化操作
+	}
 
-    // user 是跳过 Bean 属性赋值（填入）
-    // superUser 也是完全跳过 Bean 实例化（Bean 属性赋值（填入））
-    // userHolder
+	@Override
+	public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
+		if (ObjectUtils.nullSafeEquals("user", beanName) && User.class.equals(bean.getClass())) {
+			User user = (User) bean;
+			user.setId(2L);
+			user.setName("shui4");
+			// "user" 对象不允许属性赋值（填入）（配置元信息 -> 属性值）
+			return false;
+		}
+		return true;
+	}
 
-    @Override
-    public PropertyValues postProcessProperties(PropertyValues pvs, Object bean, String beanName)
-            throws BeansException {
-        // 对 "userHolder" Bean 进行拦截
-        if (ObjectUtils.nullSafeEquals("userHolder", beanName) && UserHolder.class.equals(bean.getClass())) {
-            // 假设 <property name="number" value="1" /> 配置的话，那么在 PropertyValues 就包含一个 PropertyValue(number=1)
+	// user 是跳过 Bean 属性赋值（填入）
+	// superUser 也是完全跳过 Bean 实例化（Bean 属性赋值（填入））
+	// userHolder
 
-            final MutablePropertyValues propertyValues;
+	@Override
+	public PropertyValues postProcessProperties(PropertyValues pvs, Object bean, String beanName)
+			throws BeansException {
+		// 对 "userHolder" Bean 进行拦截
+		if (ObjectUtils.nullSafeEquals(BEAN_NAME_USER_HOLDER, beanName) && UserHolder.class.equals(bean.getClass())) {
+			// 假设 <property name="number" value="1" /> 配置的话，那么在 PropertyValues 就包含一个 PropertyValue(number=1)
 
-            if (pvs instanceof MutablePropertyValues) {
-                propertyValues = (MutablePropertyValues) pvs;
-            } else {
-                propertyValues = new MutablePropertyValues();
-            }
+			final MutablePropertyValues propertyValues;
 
-            // 等价于 <property name="number" value="1" />
-            propertyValues.addPropertyValue("number", "1");
-            // 原始配置 <property name="description" value="The user holder" />
+			if (pvs instanceof MutablePropertyValues) {
+				propertyValues = (MutablePropertyValues) pvs;
+			} else {
+				propertyValues = new MutablePropertyValues();
+			}
 
-            // 如果存在 "description" 属性配置的话
-            if (propertyValues.contains("description")) {
-                // PropertyValue value 是不可变的
+			// 等价于 <property name="number" value="1" />
+			propertyValues.addPropertyValue("number", "1");
+			// 原始配置 <property name="description" value="The user holder" />
+
+			// 如果存在 "description" 属性配置的话
+			if (propertyValues.contains("description")) {
+				// PropertyValue value 是不可变的
 //                    PropertyValue propertyValue = propertyValues.getPropertyValue("description");
-                propertyValues.removePropertyValue("description");
-                propertyValues.addPropertyValue("description", "The user holder V2");
-            }
+				propertyValues.removePropertyValue("description");
+				propertyValues.addPropertyValue("description", "The user holder V2");
+			}
 
-            return propertyValues;
-        }
-        return null;
-    }
+			return propertyValues;
+		}
+		return null;
+	}
 
-    @Override
-    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        if (ObjectUtils.nullSafeEquals("userHolder", beanName) && UserHolder.class.equals(bean.getClass())) {
-            UserHolder userHolder = (UserHolder) bean;
-            // UserHolder description = "The user holder V2"
-            userHolder.setDescription("The user holder V3");
-        }
-        return bean;
-    }
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		System.out.println("postProcessBeforeInitialization");
+		if (ObjectUtils.nullSafeEquals(BEAN_NAME_USER_HOLDER, beanName) && UserHolder.class.equals(bean.getClass())) {
+			UserHolder userHolder = (UserHolder) bean;
+			// UserHolder description = "The user holder V2"
+			userHolder.setDescription("The user holder V3");
+		}
+		return bean;
+	}
 
-    @Override
-    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
-        if (ObjectUtils.nullSafeEquals("userHolder", beanName) && UserHolder.class.equals(bean.getClass())) {
-            UserHolder userHolder = (UserHolder) bean;
-            // init() = The user holder V6
-            // UserHolder description = "The user holder V6"
-            userHolder.setDescription("The user holder V7");
-        }
-        return bean;
-    }
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		if (ObjectUtils.nullSafeEquals("userHolder", beanName) && UserHolder.class.equals(bean.getClass())) {
+			UserHolder userHolder = (UserHolder) bean;
+			// init() = The user holder V6
+			// UserHolder description = "The user holder V6"
+			userHolder.setDescription("The user holder V7");
+		}
+		return bean;
+	}
+
+
 }
