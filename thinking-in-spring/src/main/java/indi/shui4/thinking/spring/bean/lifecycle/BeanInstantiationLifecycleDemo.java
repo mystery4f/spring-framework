89761984befd21c9be2e328abc3185aa6abc99b5
shui4 <1078185271@qq.com>
2023-07-09 10:40:12 +0800

    feat: Aware

--- thinking-in-spring/src/main/java/indi/shui4/thinking/spring/bean/lifecycle/BeanInstantiationLifecycleDemo.java
+++ thinking-in-spring/src/main/java/indi/shui4/thinking/spring/bean/lifecycle/BeanInstantiationLifecycleDemo.java
@@ -7,7 +7,11 @@ import org.springframework.beans.PropertyValues;
 import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.util.ObjectUtils;
+
+import static indi.shui4.thinking.spring.bean.lifecycle.MyInstantiationAwareBeanPostProcessor.SUPER_USER;
+
 
 /**
  * Bean 实例化生命周期示例
@@ -17,76 +21,102 @@ import org.springframework.util.ObjectUtils;
 @SuppressWarnings("squid:S125")
 public class BeanInstantiationLifecycleDemo {
 
-	private static final String SUPER_USER = "superUser";
 
 	public static void main(String[] args) {
+//		executeBeanFactory();
+		executeApplicationContext();
+	}
+
+
+	public static void executeBeanFactory() {
 		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
 		// 添加BeanPostProcessor 实现（示例）
-		beanFactory.addBeanPostProcessor(new MyInstantiationAwareBeanPostProcessor());
+		// - 方法1
+		// beanFactory.addBeanPostProcessor(new MyInstantiationAwareBeanPostProcessor());
+		// - 方法2
+		// <bean class="indi.shui4.thinking.spring.bean.lifecycle.MyInstantiationAwareBeanPostProcessor"/>
+
 		XmlBeanDefinitionReader beanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
 		String[] locations = {"classpath:META-INF/dependency-lookup-context.xml", "classpath:META-INF/bean-constructor-dependency-injection.xml"};
 		beanDefinitionReader.loadBeanDefinitions(locations);
 		// 构造器注入按照类型注入，resolveDependency
 		System.out.println(beanFactory.getBean("userHolder"));
 		System.out.println(beanFactory.getBean(SUPER_USER));
+
 	}
 
-	public static class MyInstantiationAwareBeanPostProcessor implements InstantiationAwareBeanPostProcessor {
+	public static void executeApplicationContext() {
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext();
+		// 添加BeanPostProcessor 实现（示例）
+		String[] locations = {"classpath:META-INF/dependency-lookup-context.xml", "classpath:META-INF/bean-constructor-dependency-injection.xml"};
+		applicationContext.setConfigLocations(locations);
+		// 启动应用上下文
+		applicationContext.refresh();
+		// 构造器注入按照类型注入，resolveDependency
+		System.out.println(applicationContext.getBean("userHolder"));
+		System.out.println(applicationContext.getBean(SUPER_USER));
+		// 关闭应用上下文
+		applicationContext.close();
+	}
 
-		public static final String FIELD_DESCRIPTION = "description";
+}
 
-		private static boolean isSupperUser(Class<?> beanClass, String beanName) {
-			return ObjectUtils.nullSafeEquals(SUPER_USER, beanName) && SuperUser.class.equals(beanClass);
-		}
+class MyInstantiationAwareBeanPostProcessor implements InstantiationAwareBeanPostProcessor {
+
+	public static final String FIELD_DESCRIPTION = "description";
+	public static final String SUPER_USER = "superUser";
+
+	private static boolean isSupperUser(Class<?> beanClass, String beanName) {
+		return ObjectUtils.nullSafeEquals(SUPER_USER, beanName) && SuperUser.class.equals(beanClass);
+	}
 
 
-		@Override
-		public Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException {
-			if (isSupperUser(beanClass, beanName)) {
-				// 把配置完成 superUser 覆盖
-				// return new SuperUser();
-				return null;
-			}
-			// 保持 Spring IoC 容器实例化操作
+	@Override
+	public Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) throws BeansException {
+		if (isSupperUser(beanClass, beanName)) {
+			// 把配置完成 superUser 覆盖
+			// return new SuperUser();
 			return null;
 		}
+		// 保持 Spring IoC 容器实例化操作
+		return null;
+	}
 
-		@Override
-		public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
-			if (isSupperUser(bean.getClass(), beanName)) {
-				SuperUser user = (SuperUser) bean;
-				user.setId(System.currentTimeMillis());
-				user.setName("shui4");
-				// 对象不允许属性赋值
-				return false;
-			}
-			return true;
+	@Override
+	public boolean postProcessAfterInstantiation(Object bean, String beanName) throws BeansException {
+		if (isSupperUser(bean.getClass(), beanName)) {
+			SuperUser user = (SuperUser) bean;
+			user.setId(System.currentTimeMillis());
+			user.setName("shui4");
+			// 对象不允许属性赋值
+			return false;
 		}
+		return true;
+	}
 
-		@Override
-		public PropertyValues postProcessProperties(PropertyValues pvs, Object bean, String beanName) throws BeansException {
-			if (ObjectUtils.nullSafeEquals("userHolder", beanName) && UserHolder.class.equals(bean.getClass())) {
-				MutablePropertyValues propertyValues;
-				if (pvs instanceof MutablePropertyValues) {
-					propertyValues = ((MutablePropertyValues) pvs);
-				} else {
-					propertyValues = new MutablePropertyValues();
+	@Override
+	public PropertyValues postProcessProperties(PropertyValues pvs, Object bean, String beanName) throws BeansException {
+		if (ObjectUtils.nullSafeEquals("userHolder", beanName) && UserHolder.class.equals(bean.getClass())) {
+			MutablePropertyValues propertyValues;
+			if (pvs instanceof MutablePropertyValues) {
+				propertyValues = ((MutablePropertyValues) pvs);
+			} else {
+				propertyValues = new MutablePropertyValues();
 
-				}
-				// 假设 <property name="number" value="1"/> 配置的话，那么在 PropertyValues 就包含一个 PropertyValues(number=1)
-				// 等价于 <property name="number" value="1"/>
-				propertyValues.addPropertyValue("number", "1");
-				// 原始配置 <property name="description" value="test"/>
+			}
+			// 假设 <property name="number" value="1"/> 配置的话，那么在 PropertyValues 就包含一个 PropertyValues(number=1)
+			// 等价于 <property name="number" value="1"/>
+			propertyValues.addPropertyValue("number", "1");
+			// 原始配置 <property name="description" value="test"/>
 
-				if (propertyValues.contains(FIELD_DESCRIPTION)) {
-					// 因为 value 是不可变的
+			if (propertyValues.contains(FIELD_DESCRIPTION)) {
+				// 因为 value 是不可变的
 //					PropertyValue description = propertyValues.getPropertyValue("description");
-					propertyValues.removePropertyValue(FIELD_DESCRIPTION);
-					propertyValues.addPropertyValue(FIELD_DESCRIPTION, "The User Holder V2");
-				}
-				return propertyValues;
+				propertyValues.removePropertyValue(FIELD_DESCRIPTION);
+				propertyValues.addPropertyValue(FIELD_DESCRIPTION, "The User Holder V2");
 			}
-			return null;
+			return propertyValues;
 		}
+		return null;
 	}
 }
--- thinking-in-spring/src/main/java/indi/shui4/thinking/spring/bean/lifecycle/UserHolder.java
+++ thinking-in-spring/src/main/java/indi/shui4/thinking/spring/bean/lifecycle/UserHolder.java
@@ -17,14 +17,24 @@
 package indi.shui4.thinking.spring.bean.lifecycle;
 
 import indi.shui4.thinking.spring.ioc.overview.domain.User;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanClassLoaderAware;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
 
 /**
  * User Holder 类
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
- * @since
  */
-public class UserHolder {
+public class UserHolder implements BeanNameAware, BeanClassLoaderAware,
+		BeanFactoryAware,
+
+
+		EnvironmentAware {
 
 	private final User user;
 	/**
@@ -35,9 +45,23 @@ public class UserHolder {
 	 * description
 	 */
 	private String description;
+	private String beanName;
+	private ClassLoader classLoader;
+	private BeanFactory beanFactory;
+	private Environment environment;
 
 	public UserHolder(User user) {
 		this.user = user;
+	}
+
+	@Override
+	public String toString() {
+		return "UserHolder{" +
+				"user=" + user +
+				", number=" + number +
+				", description='" + description + '\'' +
+				", beanName='" + beanName + '\'' +
+				'}';
 	}
 
 	public Integer getNumber() {
@@ -57,12 +81,22 @@ public class UserHolder {
 	}
 
 	@Override
-	public String toString() {
-		return "UserHolder{" +
-				"user=" + user +
-				", number=" + number +
-				", description='" + description + '\'' +
-				'}';
+	public void setBeanClassLoader(ClassLoader classLoader) {
+		this.classLoader = classLoader;
 	}
 
+	@Override
+	public void setBeanName(String name) {
+		this.beanName = name;
+	}
+
+	@Override
+	public void setEnvironment(Environment environment) {
+		this.environment = environment;
+	}
+
+	@Override
+	public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+		this.beanFactory = beanFactory;
+	}
 }
