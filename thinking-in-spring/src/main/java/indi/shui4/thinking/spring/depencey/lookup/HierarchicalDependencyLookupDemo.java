803999639f2177749de3c866a626b5f15bf9c387
shui4 <1078185271@qq.com>
2023-09-10 10:59:56 +0800

    1

--- thinking-in-spring/src/main/java/indi/shui4/thinking/spring/depencey/lookup/HierarchicalDependencyLookupDemo.java
+++ thinking-in-spring/src/main/java/indi/shui4/thinking/spring/depencey/lookup/HierarchicalDependencyLookupDemo.java
@@ -14,53 +14,53 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
  * @author shui4
  */
 public class HierarchicalDependencyLookupDemo {
-    public static void main(String[] args) {
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
-        applicationContext.register(HierarchicalDependencyLookupDemo.class);
-        ConfigurableListableBeanFactory beanFactory = applicationContext.getBeanFactory();
-        DefaultListableBeanFactory parentBeanFactory = createParentBeanFactory();
-        beanFactory.setParentBeanFactory(parentBeanFactory);
-        System.out.println("当前 BeanFactory 的 Parent BeanFactory" + beanFactory.getParentBeanFactory());
-        applicationContext.refresh();
-        displayLocalBean(beanFactory, "user");
-        displayLocalBean(parentBeanFactory, "user");
-        displayContainsBean(beanFactory, "user");
-        displayContainsBean(parentBeanFactory, "user");
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+		applicationContext.register(HierarchicalDependencyLookupDemo.class);
+		ConfigurableListableBeanFactory beanFactory = applicationContext.getBeanFactory();
+		DefaultListableBeanFactory parentBeanFactory = createParentBeanFactory();
+		beanFactory.setParentBeanFactory(parentBeanFactory);
+		System.out.println("当前 BeanFactory 的 Parent BeanFactory" + beanFactory.getParentBeanFactory());
+		applicationContext.refresh();
+		displayLocalBean(beanFactory, "user");
+		displayLocalBean(parentBeanFactory, "user");
+		displayContainsBean(beanFactory, "user");
+		displayContainsBean(parentBeanFactory, "user");
 
-        applicationContext.close();
-    }
+		applicationContext.close();
+	}
 
-    private static DefaultListableBeanFactory createParentBeanFactory() {
-        DefaultListableBeanFactory parentBeanFactory = new DefaultListableBeanFactory();
-        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(parentBeanFactory);
-        reader.loadBeanDefinitions("classpath:META-INF/dependency-lookup-context.xml");
-        return parentBeanFactory;
-    }
+	private static DefaultListableBeanFactory createParentBeanFactory() {
+		DefaultListableBeanFactory parentBeanFactory = new DefaultListableBeanFactory();
+		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(parentBeanFactory);
+		reader.loadBeanDefinitions("classpath:META-INF/dependency-lookup-context.xml");
+		return parentBeanFactory;
+	}
 
-    private static void displayLocalBean(HierarchicalBeanFactory beanFactory, String beanName) {
-        System.out.printf("当前 BeanFactory[%s] 是否包含 bean[name : %s] : %s\n", beanFactory, beanName, beanFactory.containsLocalBean(beanName));
+	private static void displayLocalBean(HierarchicalBeanFactory beanFactory, @SuppressWarnings("SameParameterValue") String beanName) {
+		System.out.printf("当前 BeanFactory[%s] 是否包含 bean[name : %s] : %s%n", beanFactory, beanName, beanFactory.containsLocalBean(beanName));
 
-    }
+	}
 
-    /**
-     * 双亲委派
-     *
-     * @param beanFactory beanFactory
-     * @param beanName    beanName
-     */
-    private static void displayContainsBean(HierarchicalBeanFactory beanFactory, String beanName) {
-        System.out.printf("BeanFactory[%s] 是否包含 bean[name : %s] : %s\n", beanFactory, beanName, containsBean(beanFactory, beanName));
-    }
+	/**
+	 * 双亲委派
+	 *
+	 * @param beanFactory beanFactory
+	 * @param beanName    beanName
+	 */
+	private static void displayContainsBean(HierarchicalBeanFactory beanFactory, @SuppressWarnings("SameParameterValue") String beanName) {
+		System.out.printf("BeanFactory[%s] 是否包含 bean[name : %s] : %s%n", beanFactory, beanName, containsBean(beanFactory, beanName));
+	}
 
-    private static boolean containsBean(HierarchicalBeanFactory beanFactory, String beanName) {
-        BeanFactory parentBeanFactory = beanFactory.getParentBeanFactory();
-        if (parentBeanFactory instanceof HierarchicalBeanFactory) {
-            HierarchicalBeanFactory parentHierarchicalBeanFactory = (HierarchicalBeanFactory) parentBeanFactory;
-            if (containsBean(parentHierarchicalBeanFactory, beanName)) {
-                return true;
-            }
-        }
-        return beanFactory.containsLocalBean(beanName);
-    }
+	private static boolean containsBean(HierarchicalBeanFactory beanFactory, String beanName) {
+		BeanFactory parentBeanFactory = beanFactory.getParentBeanFactory();
+		if (parentBeanFactory instanceof HierarchicalBeanFactory) {
+			HierarchicalBeanFactory parentHierarchicalBeanFactory = (HierarchicalBeanFactory) parentBeanFactory;
+			if (containsBean(parentHierarchicalBeanFactory, beanName)) {
+				return true;
+			}
+		}
+		return beanFactory.containsLocalBean(beanName);
+	}
 
 }
