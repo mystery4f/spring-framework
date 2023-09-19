7aadf621a3ebb9861165f2424e4503d8ffdc2635
shui4 <1078185271@qq.com>
2023-09-19 22:18:15 +0800

    feat: 1

--- thinking-in-spring/src/main/java/indi/shui4/thinking/spring/depencey/injection/QualifierAnnotationDependencyInjectionDemo.java
+++ thinking-in-spring/src/main/java/indi/shui4/thinking/spring/depencey/injection/QualifierAnnotationDependencyInjectionDemo.java
@@ -20,82 +20,82 @@ import java.util.Collection;
  */
 public class QualifierAnnotationDependencyInjectionDemo {
 
-    @Autowired
-    private User user;  // superUser -> primary user
+	@Autowired
+	private User user;  // superUser -> primary user
 
-    @Autowired
-    @Qualifier("user")
-    private User namedUser;
+	@Autowired
+	@Qualifier("user")
+	private User namedUser;
 
-    @Autowired
-    private Collection<User> allUser; // 2 Beans    user + supperUser
+	@Autowired
+	private Collection<User> allUser; // 2 Beans    user + supperUser
 
-    @Autowired
-    @Qualifier
-    private Collection<User> qualifierUsers; // 2 Beans user1 + user2
+	@Autowired
+	@Qualifier
+	private Collection<User> qualifierUsers; // 2 Beans user1 + user2
 
-    @Autowired
-    @UserGroup
-    private Collection<User> groupedUsers;
-
-
-    public static void main(String[] args) {
-        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
-        applicationContext.register(QualifierAnnotationDependencyInjectionDemo.class);
-
-        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(applicationContext);
-        reader.loadBeanDefinitions("classpath:/META-INF/dependency-lookup-context.xml");
-
-        applicationContext.refresh();
+	@Autowired
+	@UserGroup
+	private Collection<User> groupedUsers;
 
 
-        QualifierAnnotationDependencyInjectionDemo demo = applicationContext.getBean(QualifierAnnotationDependencyInjectionDemo.class);
-        // 期待输入 supperUser Bean
-        System.out.println(demo.user);
-        // 期待输入 user Bean
-        System.out.println(demo.namedUser);
-        // 期待输出 2 个 Bean superUser user
-        System.out.println(demo.allUser.size());
-        // 期待输出 4 个 Bean user1 user2
-        System.out.println(demo.qualifierUsers.size());
-        // 期待输出 2 个 Bean user3 user4
-        System.out.println(demo.groupedUsers.size());
-        applicationContext.close();
-    }
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+		applicationContext.register(QualifierAnnotationDependencyInjectionDemo.class);
 
-    // 整体定义上下文存在 5 个 User 类型的 Bean；
-    // supperUser
-    // user
-    // user1 -> @Qualifier
-    // user2 -> @Qualifier
-    // user3 -> @UserGroup
-    @Bean
-    @Qualifier // 进行逻辑分组
-    public User user1() {
-        return createUser("007");
-    }
+		XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(applicationContext);
+		reader.loadBeanDefinitions("classpath:/META-INF/dependency-lookup-context.xml");
 
-    private static User createUser(String number) {
-        User user = new User();
-        user.setId(number);
-        return user;
-    }
+		applicationContext.refresh();
 
-    @Bean
-    @Qualifier
-    public User user2() {
-        return createUser("008");
-    }
 
-    @Bean
-    @UserGroup
-    public User user3() {
-        return createUser("009");
-    }
+		QualifierAnnotationDependencyInjectionDemo demo = applicationContext.getBean(QualifierAnnotationDependencyInjectionDemo.class);
+		// 期待输入 supperUser Bean
+		System.out.println("demo.user:" + demo.user);
+		// 期待输入 user Bean
+		System.out.println("demo.namedUser:" + demo.namedUser);
+		// 期待输出 2 个 Bean superUser user
+		System.out.println("demo.allUser.size()：" + demo.allUser.size());
+		// 期待输出 2 个 Bean user1 user2
+		System.out.println("demo.qualifierUsers.size():" + demo.qualifierUsers.size());
+		// 期待输出 2 个 Bean user3 user4
+		System.out.println("demo.groupedUsers.size():" + demo.groupedUsers.size());
+		applicationContext.close();
+	}
 
-    @Bean
-    @UserGroup
-    public User user4() {
-        return createUser("010");
-    }
+	// 整体定义上下文存在 5 个 User 类型的 Bean；
+	// supperUser
+	// user
+	// user1 -> @Qualifier
+	// user2 -> @Qualifier
+	// user3 -> @UserGroup
+	@Bean
+	@Qualifier // 进行逻辑分组
+	public User user1() {
+		return createUser("007");
+	}
+
+	private static User createUser(String number) {
+		User user = new User();
+		user.setId(number);
+		return user;
+	}
+
+	@Bean
+	@Qualifier
+	public User user2() {
+		return createUser("008");
+	}
+
+	@Bean
+	@UserGroup
+	public User user3() {
+		return createUser("009");
+	}
+
+	@Bean
+	@UserGroup
+	public User user4() {
+		return createUser("010");
+	}
 }
