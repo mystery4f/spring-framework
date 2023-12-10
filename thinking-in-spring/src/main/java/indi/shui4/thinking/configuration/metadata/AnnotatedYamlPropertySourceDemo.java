acda659bc98c1a52f609bcdf5893694eea4ba656
shui4 <1078185271@qq.com>
2023-12-10 22:42:07 +0800

    1

---
+++ thinking-in-spring/src/main/java/indi/shui4/thinking/configuration/metadata/AnnotatedYamlPropertySourceDemo.java
@@ -0,0 +1,36 @@
+package indi.shui4.thinking.configuration.metadata;
+
+import indi.shui4.thinking.spring.ioc.overview.domain.User;
+import indi.shui4.thinking.spring.ioc.overview.enums.City;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * @author shui4
+ */
+@PropertySource(name = "yamlPropertySource", value = "classpath:/META-INF/user.yml", factory = YamlPropertySourceFactory.class)
+public class AnnotatedYamlPropertySourceDemo {
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+		applicationContext.register(AnnotatedYamlPropertySourceDemo.class);
+		applicationContext.refresh();
+		User user = applicationContext.getBean(User.class);
+		System.out.println(user);
+		applicationContext.close();
+	}
+
+	@Bean
+	public User user(@Value("${user.id}") String id, @Value("${user.name}") String name,
+					 @Value("${user.city}") City city
+	) {
+		User user = new User();
+		user.setId(id);
+		user.setName(name);
+		user.setCity(city);
+		return user;
+	}
+
+
+}
---
+++ thinking-in-spring/src/main/java/indi/shui4/thinking/configuration/metadata/XmlBasedYamlPropertySourceDemo.java
@@ -0,0 +1,21 @@
+package indi.shui4.thinking.configuration.metadata;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+
+import java.util.Map;
+
+/**
+ * @author shui4
+ */
+
+@SuppressWarnings("unchecked")
+public class XmlBasedYamlPropertySourceDemo {
+	public static void main(String[] args) {
+		DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+		XmlBeanDefinitionReader xmlBeanDefinitionReader = new XmlBeanDefinitionReader(beanFactory);
+		xmlBeanDefinitionReader.loadBeanDefinitions("META-INF/yaml-property-source-context.xml");
+		Map<String, Object> yamlMap = beanFactory.getBean("yamlMap", Map.class);
+		System.out.println(yamlMap);
+	}
+}
