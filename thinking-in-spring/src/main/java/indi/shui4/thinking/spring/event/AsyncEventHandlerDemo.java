3376ac0df76d6e405eff255d68d6c5dcb7c7dae1
shui4 <1078185271@qq.com>
2024-04-07 21:43:27 +0800

    feat: Spring 4.1 事件异常处理

--- thinking-in-spring/src/main/java/indi/shui4/thinking/spring/event/AsyncEventHandlerDemo.java
+++ thinking-in-spring/src/main/java/indi/shui4/thinking/spring/event/AsyncEventHandlerDemo.java
@@ -1,5 +1,6 @@
 package indi.shui4.thinking.spring.event;
 
+import cn.hutool.core.lang.Console;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.ContextClosedEvent;
@@ -20,19 +21,24 @@ public class AsyncEventHandlerDemo {
 	public static void main(String[] args) {
 		final var applicationContext = new GenericApplicationContext();
 		applicationContext.addApplicationListener(new MySpringEventListener());
+		applicationContext.addApplicationListener((ApplicationListener<MySpringEvent>) event -> {
+			throw new RuntimeException("故意抛出异常");
+		});
 		applicationContext.refresh();
 		final var applicationEventMulticaster = applicationContext.getBean(APPLICATION_EVENT_MULTICASTER_BEAN_NAME,
 				ApplicationEventMulticaster.class
 		);
 		final var taskExecutor = Executors.newSingleThreadExecutor();
-		if (applicationEventMulticaster
-				instanceof SimpleApplicationEventMulticaster) {
+		if (applicationEventMulticaster instanceof SimpleApplicationEventMulticaster) {
 			SimpleApplicationEventMulticaster eventMulticaster = (SimpleApplicationEventMulticaster) applicationEventMulticaster;
 			eventMulticaster.setTaskExecutor(taskExecutor);
 			eventMulticaster.addApplicationListener((ApplicationListener<ContextClosedEvent>) event -> {
 				if (!taskExecutor.isShutdown()) {
 					taskExecutor.shutdown();
 				}
+			});
+			eventMulticaster.setErrorHandler(t -> {
+				Console.error("异常了", t.getCause());
 			});
 		}
 		applicationContext.publishEvent(new MySpringEvent("Hello,World"));
